31f6400b143f311ef5191ff070a75b12a257a9cd
Prajwal Agrawal <findprajju@gmail.com>
2024-10-25 14:00:16 -0700

    first commit test

---
+++ main.py
@@ -0,0 +1,9 @@
+import geocoder
+import skyfield.api
+
+def get_current_gps_coordinates():
+    g = geocoder.ip('me')#this function is used to find the current information using our IP Add
+    if g.latlng is not None: #g.latlng tells if the coordiates are found or not
+        return g.latlng
+    else:
+        return None
